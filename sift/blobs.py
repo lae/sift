96be7d7c7c799c19aefb5e3faf9a13eae09c5d54
lae <lae@idolactiviti.es>
2015-06-29 05:34:58 +0000

    PEP8 blobs.py

--- sift/blobs.py
+++ sift/blobs.py
@@ -18,6 +18,8 @@ if sift.config['MEMCACHE_ENABLED']:
             }
         }
     )
+    # We use this to allow us to run multiple instances with separate databases
+    # i.e. EN and JP need to use a separate cache
     if 'MEMCACHE_KEY' not in sift.config:
         region.key = 'sift'
     else:
@@ -36,7 +38,13 @@ class Ranking(object):
     @region.cache_on_arguments(namespace=region.key)
     def get(self, event_id, limit, page):
         c = get_db()
-        c.execute("SELECT rank,name,user_id,score FROM rankings WHERE event_id = %(event_id)s AND step = (SELECT MAX(step) FROM rankings WHERE event_id = %(event_id)s) ORDER BY rank LIMIT %(limit)s OFFSET %(offset)s", {'event_id': event_id, 'limit': limit, 'offset': page * limit})
+        c.execute(
+            "SELECT rank,name,user_id,score FROM rankings " \
+            "WHERE event_id = %(event_id)s AND step = " \
+            "(SELECT MAX(step) FROM rankings WHERE event_id = %(event_id)s) " \
+            "ORDER BY rank LIMIT %(limit)s OFFSET %(offset)s",
+            {'event_id': event_id, 'limit': limit, 'offset': page * limit}
+        )
         rankings = c.fetchall()
         return rankings
 
@@ -44,7 +52,15 @@ class SearchUser(object):
     @region.cache_on_arguments(namespace=region.key)
     def get(self, event_id, search):
         c = get_db()
-        c.execute("SELECT event_id, rank, user_id, name FROM rankings WHERE event_id = %(event_id)s AND step = (SELECT MAX(step) FROM rankings WHERE event_id = %(event_id)s) AND lower(name) LIKE %(search)s ORDER BY rank LIMIT 500", {'event_id': event_id, 'search': '%'+search.lower()+'%'})
+        c.execute(
+            "SELECT event_id, rank, user_id, name FROM rankings " \
+            "WHERE event_id = %(event_id)s AND step = " \
+                "(SELECT MAX(step) FROM rankings " \
+                    "WHERE event_id = %(event_id)s) " \
+                "AND lower(name) LIKE %(search)s " \
+            "ORDER BY rank LIMIT 500",
+            {'event_id': event_id, 'search': '%'+search.lower()+'%'}
+        )
         results = c.fetchall()
         return results
 
@@ -52,7 +68,12 @@ class HistoryUser(object):
     @region.cache_on_arguments(namespace=region.key)
     def get(self, event_id, user_id):
         c = get_db()
-        c.execute("SELECT step,rank,name,score FROM rankings WHERE event_id = %(event_id)s AND user_id = %(user_id)s ORDER BY step", {'event_id': event_id, 'user_id': user_id})
+        c.execute(
+            "SELECT step,rank,name,score FROM rankings " \
+            "WHERE event_id = %(event_id)s AND user_id = %(user_id)s " \
+            "ORDER BY step",
+            {'event_id': event_id, 'user_id': user_id}
+        )
         history = c.fetchall()
         return history
 
@@ -60,7 +81,17 @@ class HistoryRank(object):
     @region.cache_on_arguments(namespace=region.key)
     def get(self, event_id, rank):
         c = get_db()
-        c.execute("SELECT s.* FROM (SELECT step FROM rankings_mv_playercount WHERE event_id = %(event_id)s AND players >= %(rank)s ORDER BY step) v(desired_step), lateral (SELECT step, score, name FROM rankings WHERE event_id = %(event_id)s AND rank <= %(rank)s AND step = desired_step ORDER BY rank DESC LIMIT 1) s ORDER BY s.step", {'event_id': event_id, 'rank': rank})
+        c.execute(
+            "SELECT s.* FROM (SELECT step FROM rankings_mv_playercount " \
+                "WHERE event_id = %(event_id)s AND players >= %(rank)s " \
+                "ORDER BY step) v(desired_step), " \
+            "lateral (SELECT step, score, name FROM rankings " \
+                "WHERE event_id = %(event_id)s AND rank <= %(rank)s " \
+                "AND step = desired_step " \
+                "ORDER BY rank DESC LIMIT 1) s " \
+            "ORDER BY s.step",
+            {'event_id': event_id, 'rank': rank}
+        )
         history = c.fetchall()
         return history
 
@@ -68,11 +99,28 @@ class Cutoff(object):
     @region.cache_on_arguments(namespace=region.key)
     def get(self, event_id, cutoff_marks):
         c = get_db()
-        c.execute("SELECT desired_rank, s.* FROM unnest(%(ranks)s) u(desired_rank), (SELECT step, players FROM rankings_mv_playercount WHERE event_id = %(event_id)s ORDER BY step) v(desired_step, players_in_current_step), lateral (SELECT step, score, name FROM rankings WHERE event_id = %(event_id)s AND rank <= desired_rank AND step = desired_step AND desired_rank <= players_in_current_step ORDER BY rank DESC LIMIT 1) s ORDER BY s.step", {'event_id': event_id, 'ranks': cutoff_marks})
+        c.execute(
+            "SELECT sel_rank, s.* FROM unnest(%(ranks)s) u(sel_rank), " \
+            # Grab a list of steps from selected event along with player count
+            "(SELECT step, players FROM rankings_mv_playercount " \
+                "WHERE event_id = %(event_id)s ORDER BY step) " \
+                "v(sel_step, sel_step_players), " \
+            # and then try to match the closest rank we want, since ties cause
+            # duplicate rank numbers (and skips numbers), if the number of
+            # players is greater than the rank we want
+            "lateral (SELECT step, score, name FROM rankings " \
+                "WHERE event_id = %(event_id)s AND rank <= sel_rank " \
+                    "AND step = sel_step AND sel_rank <= sel_step_players " \
+                "ORDER BY rank DESC LIMIT 1) s " \
+            # and make sure we order by step (a.k.a. elapsed hours)
+            "ORDER BY s.step",
+            {'event_id': event_id, 'ranks': cutoff_marks}
+        )
         results = c.fetchall()
-        cutoffs = [{"step": step} for step in set(sorted([item['step'] for item in results]))]
+        cutoffs = [{"step": step} for step in
+                   set(sorted([item['step'] for item in results]))]
         for item in results:
-            tier = "tier_{}".format(cutoff_marks.index(int(item['desired_rank'])))
+            tier = "tier_{}".format(cutoff_marks.index(int(item['sel_rank'])))
             score = item['score']
             step = item['step']
             name = item['name']
