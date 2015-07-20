d446bcaa3f8163de5ccff6144b32add9c42b26ed
lae <lae@idolactiviti.es>
2015-07-20 04:36:25 +0000

    equality oops

--- sift/core.py
+++ sift/core.py
@@ -68,7 +68,7 @@ def list_rankings(event_id):
 
     data = Ranking().get(event_id, limit, page)
     if not data:
-        if event_id = sift.config['CURRENT_EVENT_ID']:
+        if event_id == sift.config['CURRENT_EVENT_ID']:
             return render_template(
                 'event_not_started.html',
                 event = event_info[0]
@@ -119,7 +119,7 @@ def history_rank(event_id, rank):
 
     data = HistoryRank().get(event_id, rank)
     if not data:
-        if event_id = sift.config['CURRENT_EVENT_ID']:
+        if event_id == sift.config['CURRENT_EVENT_ID']:
             return render_template(
                 'event_not_started.html',
                 event = event_info[0]
@@ -155,7 +155,7 @@ def list_cutoffs(event_id):
     data = Cutoff().get(event_id, borders)
     data = list(reversed(data))
     if not data:
-        if event_id = sift.config['CURRENT_EVENT_ID']:
+        if event_id == sift.config['CURRENT_EVENT_ID']:
             return render_template(
                 'event_not_started.html',
                 event = event_info[0]
